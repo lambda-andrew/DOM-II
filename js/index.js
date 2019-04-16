7731e987a0415e3b26a60df907b901162ae19648
Lambda_School_Loaner_99 <lambda_school_loaner_99@Lambda-School-Loaner-99s-MacBook-Pro.local>
2019-04-16 12:01:53 -0700

    logoHeading disapear on click test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+
+const logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener("click", () => {
+    event.target.style.display = "none";
+})
+
+\
+
