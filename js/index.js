82c51ce3d3eeb315b2282dcbcecaa488f3277393
efrain <efarinva98@gmail.com>
2019-04-16 16:38:41 -0400

    added comments

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
 // Your code goes here
-
+//nav
 let homeN = document.querySelectorAll(".nav-link");
-// homeN[1].addEventListener("click", event => {
-//   event.preventDefault();
-//   event.target.style.color = "red";
-//   // alert("hello");
-//   console.log("anything");
-// });
 
 for(let i = 0 ; i <=3 ;i++){
   homeN[i].addEventListener("click", event => {
@@ -25,7 +19,7 @@ for(let i = 0 ; i <=3 ;i++){
     console.log("anything");
   });
 }
-
+//button
 let but = document.querySelectorAll(".btn")
 for(let i = 0 ; i <= 2 ; i++ ){
 but[i].addEventListener("mouseover", item => {
@@ -43,4 +37,22 @@ for(let i = 0 ; i <= 2 ; i++ ){
   });
 }
 
-//////////////////////
+
+
+// Home Nav
+const homeN =
+  document.querySelector(".nav-link");
+homeN.addEventListener("click", event => {
+  event.target.style.color = "red";
+  event.preventDefault();
+});
+
+// All Paragraphs
+const p = document.querySelectorAll('p');
+const pHover = event => {
+  event.target.style.background = 'yellow';
+}
+
+p.forEach(item => {
+  item.addEventListener('mouseover', pHover)
+});
