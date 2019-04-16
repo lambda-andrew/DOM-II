cec5e2f31120579a52239c4d95041959fdb13dfb
Cory Hedeen <hedeencory@gmail.com>
2019-04-16 17:29:05 -0400

    added mouseover and mouseleave events to the anchor elements with a for loop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+let anchors = document.getElementsByTagName('a');
+let anchorArray = Array.from(anchors);
+for(let i = 0; i < anchorArray.length; i++){
+anchorArray[i].addEventListener('mouseover', (event) => {event.target.style.color = "red"});
+anchorArray[i].addEventListener('mouseleave', (event) => {event.target.style.color = "black"});
+}
+let header = document.querySelector('.logo-heading');
+header.addEventListener('keydown', (event)=>{
+  if(event.keyCode === 65){
+    event.target.style.fontSize = "5rem";
+  }
+})
