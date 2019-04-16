4869fe1eb84b7d4a9a09ed7b680913a303f9c070
BrennanHolm <brennan.mholmich@gmail.com>
2019-04-16 15:18:04 -0500

    Add nav and p events
    Changes the color of the Home link to red on click. Changes the 
    background color of all paragraphs to yellow on hover.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+// Home Nav
+const homeN =
+  document.querySelector(".nav-link");
+homeN.addEventListener("click", event => {
+  event.target.style.color = "red";
+  event.preventDefault();
+});
+
+// All Paragraphs
+const p = document.querySelectorAll('p');
+const pHover = event => {
+  event.target.style.background = 'yellow';
+}
+
+p.forEach(item => {
+  item.addEventListener('mouseover', pHover)
+});
