b69f500a8921522be3f1f3bb5274c440421cb72f
Lambda_School_Loaner_99 <lambda_school_loaner_99@Lambda-School-Loaner-99s-MacBook-Pro.local>
2019-04-16 14:08:54 -0700

    commit

--- js/index.js
+++ js/index.js
@@ -2,10 +2,17 @@
 
 
 
-const logoHeading = document.querySelector(".logo-heading");
-logoHeading.addEventListener("click", () => {
-    event.target.style.display = "none";
+
+const logoHeading = document.querySelector('.logo-heading').addEventListener('click', (e) =>{
+    event.target.textContent='sub nuf';
 })
 
-\
+const navContainer = document.querySelector('.main-navigation').addEventListener('click', (e) =>{
+    event.target.style.backgroundcolor= 'black';
+})
+
+const btn = document.querySelector('.btn').addEventListener('mouseover', (e) =>{
+    event.target.textContent='!pu em ngis';
+})
+
 
