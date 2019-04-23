e7e148abbd5ef2c52e71929fdc503a9274f012b0
Cory Hedeen <hedeencory@gmail.com>
2019-04-23 16:51:58 -0400

    Finish events, add propagation

--- js/index.js
+++ js/index.js
@@ -9,10 +9,30 @@ navItems[2].addEventListener('mouseleave', (event) => event.target.style.color =
 navItems[3].addEventListener('mouseover', (event) => event.target.style.color = "orange")
 navItems[3].addEventListener('mouseleave', (event) => event.target.style.color = "black")
 
+const logo = document.querySelector('.logo-heading')
+logo.addEventListener('click', (event) => event.target.style.fontSize = "5rem")
+logo.addEventListener('dblclick', (event) => event.target.style.fontSize = "4rem")
+logo.addEventListener('mouseover', (event) => event.target.style.cursor = "pointer")
 
-const navContainer = document.querySelector('.nav-container')
-navContainer.addEventListener('mouseover', (event) => {event.target.style.color = "grey"})
+
+const input = document.createElement('input')
+input.addEventListener('keydown', (event) => event.target.style.background = "blue")
+input.addEventListener('keyup', (event) => event.target.style.background = "orange")
+input.addEventListener('focus', (event) => {event.target.style.background = "yellow",
+event.stopPropagation()})
+logo.prepend(input)
+
 
 const busImg = document.querySelector('.container img')
 busImg.addEventListener('dragstart', (event) => event.target.style.border = "1px dashed grey")
 busImg.addEventListener('dragend', (event) => event.target.style.border = "1px dashed transparent")
+
+const firstContent = document.querySelector('.intro')
+firstContent.addEventListener('copy', () => myEvent())
+
+function myEvent(){
+  alert("OH.");
+}
+
+const firstHeader = document.querySelector('.intro h2')
+firstHeader.addEventListener('focus', (event) => event.target.style.border = "2px outset orange")
